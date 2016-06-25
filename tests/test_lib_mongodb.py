9629c3b2f57b323121d27a5e41ae2b2ed96f1eeb
Marco Ceppi <marco@ceppi.net>
2016-06-24 22:26:42 -0400

    mock out distrib

--- tests/test_lib_mongodb.py
+++ tests/test_lib_mongodb.py
@@ -42,8 +42,10 @@ class MongoDBTest(unittest.TestCase):
         lsb.side_effect = [{'DISTRIB_CODENAME': 'nogo'}]
         self.assertRaises(Exception, mongodb.mongodb, 'archive')
 
+    @patch('charms.layer.mongodb.lsb_release')
     @patch('charms.layer.mongodb.platform')
-    def test_mongodb_zseries(self, plat):
+    def test_mongodb_zseries(self, plat, lsb):
+        lsb.side_effect = [{'DISTRIB_RELEASE': '16.04'}]
         plat.machine.return_value = 's390x'
         self.assertEqual(type(mongodb.mongodb('archive')).__name__,
                          'MongoDBzSeries')
